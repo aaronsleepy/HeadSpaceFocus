2142c6106014868ba7647977f32e94e3e4e4f596
aaronsleepy <aaron@kmong.com>
2022-11-20 21:08:06 +0900

    feat: dataSource, snapshot, layout 연동

--- HeadSpaceFocus/FocusViewController.swift
+++ HeadSpaceFocus/FocusViewController.swift
@@ -11,11 +11,50 @@ class FocusViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var items: [Focus] = Focus.list
+    
+    // Datasource
+    typealias Item = Focus
+    enum Section {
+        case main
+    }
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // datasource
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FocusCell", for: indexPath) as? FocusCell else {
+                return nil
+            }
+//            cell.configure(item)
+            return cell
+        })
+        
+        // data: snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        
+        dataSource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
     }
 
-
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
 }
 
