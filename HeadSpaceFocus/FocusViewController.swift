9e57cd62f0876cf3991d452d85db0c69434be34c
aaronsleepy <aaron@kmong.com>
2022-11-20 20:51:37 +0900

    feature: Focus 모델 추가

--- HeadSpaceFocus/FocusViewController.swift
+++ HeadSpaceFocus/FocusViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class FocusViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
