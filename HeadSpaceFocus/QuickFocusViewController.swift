9a3b6567cbc49ea117d4b6ddec7141e0ea42d1fd
aaronsleepy <aaron@kmong.com>
2022-11-24 19:55:34 +0900

    feat: QuickFocus storyboard, viewController 추가

---
+++ HeadSpaceFocus/QuickFocusViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuickFocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by Aaron on 2022/11/24.
+//
+
+import UIKit
+
+class QuickFocusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
