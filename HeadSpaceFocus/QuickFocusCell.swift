9e67f5639710bc781086d2ca454a2377b20c4fc2
aaronsleepy <aaron@kmong.com>
2022-11-24 20:10:57 +0900

    feat: QuickFocusCell 연결

---
+++ HeadSpaceFocus/QuickFocusCell.swift
@@ -0,0 +1,20 @@
+//
+//  QuickFocusCell.swift
+//  HeadSpaceFocus
+//
+//  Created by Aaron on 2022/11/24.
+//
+
+import UIKit
+
+class QuickFocusCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ quickFocus: QuickFocus) {
+        thumbnailImageView.image = UIImage(named: quickFocus.imageName)
+        titleLabel.text = quickFocus.title
+        descriptionLabel.text = quickFocus.description
+    }
+}
