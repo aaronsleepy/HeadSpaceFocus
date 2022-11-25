514d546f9ce73272f8f68ca72b0cec9efecc8005
aaronsleepy <aaron@kmong.com>
2022-11-25 22:36:52 +0900

    fix: navigationItem.largeTitleDisplayMode = .never

--- HeadSpaceFocus/QuickFocusListViewController.swift
+++ HeadSpaceFocus/QuickFocusListViewController.swift
@@ -63,6 +63,8 @@ class QuickFocusListViewController: UIViewController {
         
         // Layout
         collectionView.collectionViewLayout = layout()
+        
+        self.navigationItem.largeTitleDisplayMode = .never
     }
     
     private func layout() -> UICollectionViewCompositionalLayout {
